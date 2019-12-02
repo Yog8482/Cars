45acbdfe14d403a237c987f4b49ae305b9114df3
YOGENDRA <myog33@gmail.com>
2019-12-03 01:05:39 +0530

    -Utilis changed to Utilities, Added constant file for global constants. -Added CarsMainActivity with default text -In AndroidManifest.xml added internet permission and ignore GoogleAppIndexingWarning

---
+++ app/src/main/java/com/sevenpeakssoftware/yogendra_b/utilities/constants.kt
@@ -0,0 +1,4 @@
+package com.sevenpeakssoftware.yogendra_b.utilities
+
+/** The base URL of the API */
+const val BASE_URL: String = "https://www.apphusetreach.no/application/119267/article/get_articles_list"
---
+++ app/src/main/java/com/sevenpeakssoftware/yogendra_b/view/ui/CarsMainActivity.kt
@@ -0,0 +1,13 @@
+package com.sevenpeakssoftware.yogendra_b.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sevenpeakssoftware.yogendra_b.R
+
+class CarsMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cars_main)
+    }
+}
